8c00813e234156637ba85df886203ece1b8534f7
John Caleb <Calebjohn3112@gmail.com>
2024-01-28 00:39:56 +0100

    ms

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,12 @@ const NavBar = () => {
           href='#'
           className='text-xl font-bold bg-white text-amber-700  border border-white text-center p-2 mb-2'
         >
-          <span className='flex flex-row items-center justify-center'>
+          <a
+            href='https://github.com/thejohncaleb'
+            className='flex flex-row items-center justify-center'
+          >
             <FaGithub size={35} /> <span>&nbsp; Star on GitHub</span>
-          </span>
+          </a>
         </a>
         <a
           href='#'
@@ -20,7 +23,7 @@ const NavBar = () => {
           Docs
         </a>
         <a
-          href='#'
+          href='#faq'
           className='text-xl font-medium text-white border border-white text-center p-2 hover:bg-white hover:text-amber-700 hover:font-bold'
         >
           Support & FAQs
